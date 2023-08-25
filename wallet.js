23a385375bc591f0fb2be9bfe2605380a6e28936
harsh <harshniranjan186@gmail.com>
2023-08-25 17:25:52 +0530

    account Freeze

--- wallet.js
+++ wallet.js
@@ -1,56 +1,26 @@
 const { Wallet, Client } = require("xrpl");
-const mysql = require("mysql");
 
-const ECDSA = require("xrpl").ECDSA;
-
-// MySQL database configuration
-const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "harsh",
-  database: "wallet",
-};
-
-// Create a MySQL connection pool
-const pool = mysql.createPool(dbConfig);
+const ECDSA = require("xrpl/dist/npm/ECDSA");
 
 async function createWallet() {
   let newWallet = Wallet.generate(ECDSA.ed25519);
 
   console.log(newWallet);
-
-  // Store the wallet information in the database
-  const insertQuery =
-    "INSERT INTO wallets (public_key, private_key) VALUES (?, ?)";
-  const values = [newWallet.publicKey, newWallet.privateKey];
-
-  pool.query(insertQuery, values, (error, results) => {
-    if (error) {
-      console.error("Error inserting wallet:", error);
-    } else {
-      console.log("Wallet inserted into the database:", results);
-    }
-  });
 }
 
 async function fundWallet() {
   let wallet = Wallet.fromSeed("sEdTkZy1C35mXyJAFaoQNnoQyieabbx");
+
   let client = new Client("wss://s.altnet.rippletest.net/");
 
-  try {
-    await client.connect();
+  await client.connect();
 
-    console.log("Are we connected? " + client.isConnected());
+  console.log("are we connected? " + client.isConnected());
 
-    let result = await client.fundWallet(wallet);
+  let result = await client.fundWallet(wallet);
 
-    console.log(result);
-  } catch (error) {
-    console.error("Error:", error);
-  } finally {
-    client.disconnect();
-  }
+  console.log(result);
 }
 
 createWallet();
-fundWallet();
+// fundWallet();
